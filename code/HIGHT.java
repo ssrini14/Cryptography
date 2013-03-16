f6341d72edcfade7c7a39d8b80b65641a7dc0b46
Christopher Sasarak <cms5347@rit.edu>
2013-03-16 04:15:30 -0400

    Created some initial constructors for the HIGHT object

--- code/HIGHT.java
+++ code/HIGHT.java
@@ -8,6 +8,36 @@
  * HIGHT block cipher. 
 */ 
 public class HIGHT implements BlockCipher{
+    public static final int DEFAULT_BLOCK_SIZE = 128, DEFAULT_KEY_SIZE = 64, 
+        DEFAULT_ROUNDS = 32;
+
+    private int rounds, keySize, blockSize;
+    private byte[] key, wKeys;
+
+    /**
+     * This constructor will intialize this HIGHT object
+     * with the properties given in its parameters.
+     *
+     * @param blockSize The size of each block to be encrypted
+     * @param keySize The size (in bits) of the key to give to HIGHT.
+     * @param key The key to use to encrypt the cipher-text with.
+     * @param rounds The number of rounds to run. 
+     */
+    public HIGHT(int blockSize, int keySize, byte[] key, int rounds){
+        this.keySize = keySize;
+        this.key = key;
+        this.rounds = rounds;
+        this.blockSize = blockSize;
+    }
+    
+    /**
+     * This constructor takes no arguments and will set the HIGHT
+     * object to use the default values described in the HIGHT paper.
+     * NOTE: Key needs to be specified separately with the setKey method.
+     */
+    public HIGHT(){
+        this(DEFAULT_BLOCK_SIZE, DEFAULT_KEY_SIZE, null, DEFAULT_ROUNDS);
+    }
 
     /**
 	 * Returns this block cipher's block size in bytes.
