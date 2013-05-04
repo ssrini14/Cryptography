0095d785c6e5545d333d57fb198505eb1ba10b52
Christopher Sasarak <cms5347@rit.edu>
2013-05-04 01:48:34 -0400

    Added some command line arg handline functions

--- code/attack/attack.hs
+++ code/attack/attack.hs
@@ -1,2 +1,16 @@
 -- Filename: attack.hs
 -- Author: Christopher Sasarak
+
+module Main where
+import System.Environment
+
+usage = "./attack <plaintext-ciphertext file>"
+       
+main:: IO ()
+main = do args <- getArgs
+          if null args then 
+            putStrLn usage
+          else
+            putStrLn "Do stuff here!" 
+        
+loadAndStore args = "hello"
