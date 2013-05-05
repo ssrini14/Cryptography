915872f1006d436c7f483d2ddb992349a684eaf8
Srinivas Sridharan <ssrini14@gmail.com>
2013-05-05 01:08:35 -0400

    Hopefully my last change for the day
    Updated the Generate text file for adding comments

--- code/matlab/generatetxtFile.m
+++ code/matlab/generatetxtFile.m
@@ -1,18 +1,33 @@
+%Script for generating Plain text and Cipher text pairs
+
 clc;
 clear;
-%01a3d965367112e6f482795093bc2633
-KeyHexArray = ['01'; 'a3' ;'d9' ;'65'; '36'; '71' ;'12'; 'e6'; 'f4'; '82'; '79'; '50'; '93'; 'bc'; '26'; '33'];
-fileName = reshape(KeyHexArray',1,[]).'.';
-Key =hex2dec(KeyHexArray);
-fid = fopen([fileName '.txt'], 'wt'); 
+
+%Enter the Master Key in hexadecimal
+KeyHex = '00112233445566778899aabbccddeeff';
+
+%convert from Hexadecimal to byte array
+Key = hex2dec(reshape(KeyHex,2,[]).');
+%Create a file with the same name as the Master Key
+fid = fopen([KeyHex '.txt'], 'wt'); 
+
+%instantiate the Hight class and set the key and number of rounds
 Hgt = Hight();
 Hgt.setKey(Key);
 Hgt.setRounds(1);
+
+%create 512 Plain Text and Cipher Text pairs
 for incr1= 1:1:512
+    
+   %Generate a random Plain Text 
    PT = floor(rand(1,8)*255);
+   %Perform Hight encryption
    [CT,CTHex,inputPT,PTHex] = Hgt.encrypt(PT);
+   %Have a comma seperated Plain text and Cipher text value
    value = [PTHex ',' CTHex];
+   %Write it to the file
    fprintf(fid,value);
    fprintf(fid,'\n'); 
 end    
+%Close the file
 fclose(fid);
