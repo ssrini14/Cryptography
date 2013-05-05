e8778a93828fe9f9e53eed30b2dba15ee6ca906e
Christopher Sasarak <cms5347@rit.edu>
2013-05-04 21:47:28 -0400

    Now can read in files and parse pairs

--- code/attack/attack.hs
+++ code/attack/attack.hs
@@ -2,7 +2,12 @@
 -- Author: Christopher Sasarak
 
 module Main where
+import Data.Maybe
+import Data.List
 import System.Environment
+import System.IO
+
+type Pair = (String, String)     
 
 usage = "./attack <plaintext-ciphertext file>"
        
@@ -11,6 +16,23 @@ main = do args <- getArgs
           if null args then 
             putStrLn usage
           else
-            putStrLn "Do stuff here!" 
-        
-loadAndStore args = "hello"
+            (do h <- openFile (head args) ReadMode
+                contents <- hGetContents h
+                let pairs = loadPairs contents
+                sequence_ $ map (\x -> do putStr (fst x) 
+                                          putStr ","
+                                          putStrLn (snd x)) pairs 
+            )
+
+
+loadPairs :: String -> [Pair] 
+loadPairs inp = map parsePair $ lines inp   
+
+-- Parse's a pair out of an individual line of text
+parsePair :: String -> Pair
+parsePair str = let (x, y) = span (not . (== ',')) str 
+                in
+                (x, tail y)
+                
+                
+
