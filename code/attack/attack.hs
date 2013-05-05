4fc7cc667761626302b281b7854567db72f54368
Christopher Sasarak <cms5347@rit.edu>
2013-05-05 03:21:53 -0400

    Convert pairs from hexadecimal to integer

--- code/attack/attack.hs
+++ code/attack/attack.hs
@@ -4,14 +4,16 @@
 module Main where
 import Data.Maybe
 import Data.List
+import Numeric
 import System.Environment
 import System.IO
 
 type Pair = (String, String)     
+type PCPair = (Int, Int)
 
 usage = "./attack <plaintext-ciphertext file>"
        
-main:: IO ()
+main :: IO ()
 main = do args <- getArgs
           if null args then 
             putStrLn usage
@@ -34,5 +36,22 @@ parsePair str = let (x, y) = span (not . (== ',')) str
                 in
                 (x, tail y)
                 
-                
+-- A list of all possible subkeys
+possibleKeys = [0..255]
+
+-- This function will make plain-text cipher-text byte from a Pair for testing
+-- It assumes the input Pair strings are in hexadecimal format
+makePCPairs :: Pair -> [PCPair]
+makePCPairs (l, r) = let parseHex = fst . head . readHex 
+                         makeTwos = (map parseHex) . unfoldr (\x -> let (a, b) = splitAt 2 x 
+                                                                    in 
+                                                                    if null a then Nothing
+                                                                    else Just (a, b))
+                                                                
+                         rs = makeTwos r
+                         ls = makeTwos l
+                         in
+                             zip ls rs
+
+
 
